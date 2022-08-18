f211794946ce62d27f2a03de101288747b8b7260
EgeLosing <77616044+EgeLosing@users.noreply.github.com>
2022-08-19 01:15:04 +0300

    Add Projects files.

---
+++ BanWindows11/Main.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace BanWindows11
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(200, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ban Windows 11 Upgrade";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(222, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "(sorry but this is actually a downgrade)";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(285, 97);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Ban Windows 11";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 629);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Beta 1.0.0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(222, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ProductVersion: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(222, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "TargetReleaseVersion:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(222, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(178, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "TargetReleaseVersionInfo:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(244, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(218, 37);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Registry Results:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(12, 260);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(288, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Registry Results in a less complicated way:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(702, 14);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 65);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "X";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(773, 651);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ban Windows 11 Update (oops sorry downgrade)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ BanWindows11/Main.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace BanWindows11
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            CenterLabel(label1);
+            CenterLabel(label2);
+            CenterLabel(label4);
+            CenterLabel(label5);
+            CenterLabel(label6);
+            CenterLabel(label7);
+            CenterCheckBox(checkBox1);
+            this.Load += new EventHandler(Main_Load);
+        }
+
+        private void Main_Load(object sender, EventArgs e) => Check();
+
+        private void Check()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Policies\Microsoft\Windows\WindowsUpdate"))
+                {
+                    if (key != null)
+                    {
+                        var ProductVersion = key.GetValue("ProductVersion");
+                        var TargetReleaseVersion = key.GetValue("TargetReleaseVersion");
+                        var TargetReleaseVersionInfo = key.GetValue("TargetReleaseVersionInfo");
+
+                        if (ProductVersion != null && TargetReleaseVersion != null && TargetReleaseVersionInfo != null)
+                            checkBox1.Checked = true;
+                        else
+                        {
+                            checkBox1.Checked = false;
+                            MessageBox.Show("Windows 11 Upgrade IS NOT BANNED on your PC. If you want to BAN THE UPDATE, check the Ban Windows 11 Checkbox.", "WINDOWS 11 IS NOT BANNED!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            label8.Text = "Registry Results in a less complicated way: Oh no! You don't have Windows 11 Banned.";
+                            return;
+                        }
+                        label4.Text = "ProductVersion: " + ProductVersion;
+                        label5.Text = "TargetReleaseVersion: " + TargetReleaseVersion;
+                        label6.Text = "TargetReleaseVersionInfo: " + TargetReleaseVersionInfo;
+                        CenterLabel(label4);
+                        CenterLabel(label5);
+                        CenterLabel(label6);
+                        CenterLabel(label7);
+                        label8.Text = $"Registry Results in a less complicated way: You will continue receiving feature updates until the {ProductVersion} {TargetReleaseVersionInfo}, \nafter that you will continue receiving Security Updates for that version. When a new Windows version gets \nreleased, the program will update to get latest feature updates like Windows 10 22H2.";
+                        return;
+                    }
+                    checkBox1.Checked = false;
+                    MessageBox.Show("Windows 11 Upgrade IS NOT BANNED on your PC. If you want to BAN THE UPDATE, check the Ban Windows 11 Checkbox.", "WINDOWS 11 IS NOT BANNED!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    label8.Text = "Registry Results in a less complicated way: Oh no! You don't have Windows 11 Banned.";
+                }
+            }
+            catch
+            {
+                checkBox1.Checked = false;
+                MessageBox.Show("Windows 11 Upgrade IS NOT BANNED on your PC. If you want to BAN THE UPDATE, check the Ban Windows 11 Checkbox.", "WINDOWS 11 IS NOT BANNED!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                label8.Text = "Registry Results in a less complicated way: Oh no! You don't have Windows 11 Banned.";
+            }
+        }
+
+        private void CenterLabel(Label lbl) => lbl.Location = new Point((this.Size.Width - lbl.Size.Width) / 2, lbl.Location.Y);
+
+        private void CenterCheckBox(CheckBox checkBox) => checkBox.Location = new Point((this.Size.Width - checkBox.Size.Width) / 2, checkBox.Location.Y);
+
+        private void label9_Click(object sender, EventArgs e) => Application.Exit();
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {
+                DialogResult Prompt = MessageBox.Show("Are you sure you want to enable Windows 11?", "Enable Windows 11", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (Prompt == DialogResult.Yes)
+                {
+                    using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Policies\Microsoft\Windows\WindowsUpdate", true))
+                    {
+                        key.DeleteValue("ProductVersion");
+                        key.DeleteValue("TargetReleaseVersion");
+                        key.DeleteValue("TargetReleaseVersionInfo");
+                    }
+                    MessageBox.Show("Windows 11 Enabled, you made a big mistake...", "Enabled Windows 11", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Check();
+                    return;
+                }
+                checkBox1.Checked = true;
+            }
+            else if (checkBox1.Checked == true)
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Policies\Microsoft\Windows\WindowsUpdate", true))
+                {
+                    key.SetValue("ProductVersion", "Windows 10");
+                    key.SetValue("TargetReleaseVersion", 1);
+                    key.SetValue("TargetReleaseVersionInfo", "21H2");
+                }
+                MessageBox.Show("Windows 11 Disabled, you are safe now.", "Disabled Windows 11", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                Check();
+            }
+        }
+    }
+}
